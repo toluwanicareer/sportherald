c10f1e4e402fd0abe537efad1a42deb612597023
Toluwani <abiodun.toluwanii@gmail.com>
2018-07-07 15:26:53 +0100

    'fgfg'

--- acc/models.py
+++ acc/models.py
@@ -10,3 +10,21 @@ class Profile(models.Model):
     memo_key=models.CharField(max_length=200, null=True)
     active_key=models.CharField(max_length=200, null=True)
 
+
+class Product(models.Model):
+    title=models.CharField(max_length=200, null=True)
+    manufacturer_url=models.URLField(null=True, max_length=800)
+    image_url=models.URLField(null=True, max_length=200)
+    search_term=models.CharField(null=True, max_length=200)
+    price=models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    shipping = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    in_stock = models.BooleanField(default=True)
+    sku = models.CharField(max_length=30, null=True, blank=True)
+    details = models.CharField(max_length=800, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
--- acc/views.py
+++ acc/views.py
@@ -8,7 +8,9 @@ import json
 from django.contrib.auth import login, logout
 import pdb
 from django.http import HttpResponseRedirect, Http404
-
+from bs4 import BeautifulSoup
+import urllib
+from .models import Product
 
 # Create your views here.
 
@@ -61,4 +63,162 @@ def user_logout(request):
         return HttpResponseRedirect('/')
     else:
         return Http404('Invalid url')
+
+
+
+
+'''
+def search_cap_us(query='brush'):
+    products = []
+    header = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0', }
+    params = urllib.parse.urlencode({"search_query": query})
+    search_url='http://shop.cap-us.com/search.php'
+    response = requests.get(search_url, params, headers=header)
+    soup = BeautifulSoup(response.content, "html.parser")
+    product_lists = soup.find_all("li", class_="ListView")
+    for item in product_lists:
+
+        p = Product()
+        div = item.find_all("div", class_="ProductImage")
+
+        a = div[0].find_all("a")
+        p.image_url = a[0].img["src"]
+        p.manufacturer_url = a[0]["href"]
+        #pdb.set_trace()
+        detail_div = item.find_all("div", class_="ProductDetails")
+        #pdb.set_trace()
+        title_a = detail_div[0].find_all("a")
+        p.search_term=query
+        description=item.find_all("p", class_="ProductDescription")
+        #pdb.set_trace()
+        p.details=description[0].text
+        titleprice = title_a[0].text
+        list_stuff=process_title(titleprice)
+        p.title=list_stuff[0]
+        p.price=list_stuff[1]
+        p.save()
+
+
+def process_title(titleprice):
+    titlepricelist=titleprice.split('$')
+    price=titlepricelist[1]
+    titlepricelist[1]=float(price)
+    #pdb.set_trace()
+    return titlepricelist
+
+
+
+def colparmer(query='brush'):
+    scraped_products = []
+    search_url='https://www.coleparmer.com/search'
+    params = urllib.parse.urlencode({"searchterm": query})
+    response = requests.get(search_url, params)
+    soup = BeautifulSoup(response.content, "html.parser")
+    pdb.set_trace()
+    products_div=soup.find_all("div", class_='eb-productListing')
+    product_ul=products_div[0].find_all("ul", recursive=False)
+    product_lists=product_ul[0].find_all("li", recursive=False)
+    for product in product_lists:
+        p = Product()
+        title_div=product.find_all("div", class_='eb-productname')
+        atitle=title_div[0].find_all("a")
+        p.title=atitle[0].text
+        p.manufacturer_url=atitle[0]["href"]
+        img=product.find_all("img", class_="lazy")
+        p.image_url=img[0]['data-original']
+        #pdb.set_trace()
+        #apiece=product.find_all("a", class_="btn-add-cart")
+        #p.pieces=get_piece(apiece[0].text)
+        price_range=product.find_all("span", class_="price-range")
+        p.price=process_price(price_range[0])
+        description=product.find_all("span", class_="spec-list-value-box")
+        p.details='Product Type:'+ description[0].text
+        scraped_products.append(p)
+    return scraped_products
+
+
+
+def get_piece(text):
+    text_list=text.split(' ')
+    return text_list[2]
+
+def process_price(price_range):
+    prices=price_range.text
+    prices=prices.strip()
+    prices_list=prices.split(' ')
+    price=prices_list[0]
+    price=price.replace('$','')
+    price=float(price)
+    return price
+
+def diamonddental(query):
+    scraped_products = []
+    search_url = 'http://www.diamonddentalsupplies.com/search'
+    params = urllib.parse.urlencode({"q": query})
+    response=requests.get(search_url, params)
+    soup=BeautifulSoup(response.content, "html.parser")
+    product_list=soup.find_all("div", class_="product-item")
+    for product in product_list:
+        p=Product()
+        p.title=product.h2.text
+        imgdiv=product.find_all('div', class_='picture')
+        imagea=imgdiv[0].a
+        p.manufacturer_url=imagea['href']
+        p.image_url=imagea.img['src']
+        description=product.find_all('div' , class_='description')
+        p.details=description[0].text
+        price_con=product.find_all('span', class_='price')
+        price=price_con[0].text
+        price=price.strip()
+        price_list=price.split(' ')
+        if price_list[0] == 'From':
+            price=price_list[1]
+        else:
+            price=price_list[0]
+        price = price.replace('$', '')
+        #pdb.set_trace()
+        p.price=float(price)
+        scraped_products.append(p)
+    return scraped_products
+
+def blowoutmedical(query='brush'):
+    scraped_products = []
+    search_url='https://www.blowoutmedical.com/catalogsearch/result/'
+    params = urllib.parse.urlencode({"q": query})
+    response = requests.get(search_url, params)
+    soap=BeautifulSoup(response.content, 'html.parser')
+    item_inners=soap.find_all('div', class_='item-inner')
+    for product in item_inners:
+        p=Product()
+        imagea=product.find_all('a', class_="product-image")
+        p.manufacturer_url=imagea[0]['href']
+        p.image_url=imagea[0].img['src']
+        p.title=imagea[0].img['alt']
+        price=product.find_all('span', class_='price')[0]
+        #pdb.set_trace()
+        p.price=float(price.text.strip().replace('$', ''))
+        scraped_products.append(p)
+    return scraped_products
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
